a57266d0fbf871d4efb904362d4ad9f4024d2b17
Ruben Nunez <ruben_abdias@live.com>
2022-09-29 10:30:00 -0500

    Add Chartjs and register plugins

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Ruben</p>
+        <h1>Dashboard</h1>
       </header>
     </div>
   );
